31a4d97fb057e227bbaff6fcafc70a765edb5d96
CptVane <tia.vivi@gmail.com>
2024-12-28 18:06:51 +0100

    Version 0.1

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def handle_emergency_mode():
 create_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.getenv('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
